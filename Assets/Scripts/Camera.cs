3f851aebd3a3c2488df6e13ba1c108c69dd31a5c
Ho Minh Tri <hominhtri0599@gmail.com>
2020-09-27 23:21:36 +0700

    Commit to upload to GitHub

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+
+    public GameObject ground;
+
+    private bool generatedGround;
+
+    private System.Random random;
+
+    void Start()
+    {
+        generatedGround = false;
+
+        random = new System.Random();
+    }
+
+    void Update()
+    {
+        Vector3 cameraPosition = transform.position;
+        Vector3 playerPosition = player.position;
+
+        transform.position =
+            new Vector3(playerPosition.x, cameraPosition.y, playerPosition.z - 10);
+
+        int iteration = (int) transform.position.z / 40 + 2;
+        float relativeZ = transform.position.z % 40;
+        
+        if (relativeZ > 1 && !generatedGround)
+        {
+            float nextX = (float) (random.NextDouble() * 2 - 1) * 7;
+            float nextY = 0;
+            float nextZ = iteration * 40;
+
+            Instantiate(ground, new Vector3(nextX, nextY, nextZ), Quaternion.identity);
+
+            generatedGround = true;
+        }
+
+        if (relativeZ < 1)
+            generatedGround = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    public AudioClip jumpAudio;
+
+    private Rigidbody body;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+            Application.Quit();
+
+        if (transform.position.y < -10)
+            Retry();
+        
+        scoreText.text = ((int) transform.position.z).ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float sidewaysForce = 20;
+        float maxSidewaysVelocity = 5;
+
+        float forwardForce = 30;
+        float maxForwardVelocity = 8;
+
+        float verticalForce = 100;
+
+        float forceX = 0;
+
+		if (Input.GetKey("left"))
+			forceX = -sidewaysForce;
+		
+		if (Input.GetKey("right"))
+			forceX = sidewaysForce;
+
+        if (Math.Abs(body.velocity.x) > maxSidewaysVelocity)
+			forceX = 0;
+        
+        float forceZ = 0;
+
+        /*
+        if (Input.GetKey("up"))
+			forceZ = horizontalForce;
+		
+		if (Input.GetKey("down"))
+			forceZ = -horizontalForce;
+        */
+
+        forceZ = forwardForce;
+
+        if (Math.Abs(body.velocity.z) > maxForwardVelocity)
+			forceZ = 0;
+        
+        float forceY = 0;
+
+        if (Input.GetKey("space") && IsGrounded())
+        {
+            forceY = verticalForce;
+
+            audioSource.PlayOneShot(jumpAudio);
+        }
+
+        body.AddForce(forceX, forceY, forceZ);
+    }
+
+    void Retry()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+		SceneManager.LoadScene(scene.name);
+    }
+
+    bool IsGrounded()
+	{
+		LayerMask mask = ~(1 << 8);
+
+		return Physics.Raycast(transform.position, Vector3.down, 1.5f, mask);
+	}
+}
